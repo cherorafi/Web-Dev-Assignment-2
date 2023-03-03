64cb811117335b67608359bae442cb7e7458dd33
cherorafi <saidun2001@gmail.com>
2023-03-03 17:40:50 -0500

    created method 2

--- methods.js
+++ methods.js
@@ -9,8 +9,24 @@ Array.prototype.myMap = function (func)
     return newList;
 };
 
-//For testing function 1
-/*
+/* // For testing function 1
 const array1 = [1, 4, 9, 16];
 console.log(array1.myMap(x => x * 2));
+*/
+
+// 2. filter()
+Array.prototype.myFilter = function (func) 
+{
+    var newList = [];
+    var length = this.length
+    for (let i = 0; i < length; i++) {
+        if (func(this[i], i, this))
+            newList.push(this[i]);
+    }
+    return newList;
+};
+
+/* // For testing function 2
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+console.log(words.myFilter(word => word.length > 6));
 */
